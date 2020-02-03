a9c88f5f5d37fb238433526253a15e2a2bb12994
Barak <bachillah@gmail.com>
2020-02-03 23:13:13 +0300

    added pesapal component

---
+++ src/app/pesapal-payment/pesapal-payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pesapal-payment',
+  templateUrl: './pesapal-payment.component.html',
+  styleUrls: ['./pesapal-payment.component.css']
+})
+export class PesapalPaymentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
