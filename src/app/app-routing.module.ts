1857ec088a9163d5a48ef4e05d925a305b3d7bfe
Barak <bachillah@gmail.com>
2020-02-03 22:41:19 +0300

    added admin and pesapal routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SignUpComponent} from './sign-up/sign-up.component';
+import {PesapalPaymentComponent} from './pesapal-payment/pesapal-payment.component';
+import {AdminComponent} from './admin/admin.component';
 
 
 const routes: Routes = [
@@ -9,6 +11,8 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   { path: 'sign-up', component: SignUpComponent},
+  { path: 'pesapal', component: PesapalPaymentComponent},
+  { path: 'admin', component: AdminComponent},
   ]
 
 @NgModule({
